e3bb4f64c50e9295f5135feb1dfb154efcc2e074
ldw1986hf123@163.com <1234@qq.com>
2019-08-31 00:11:15 +0800

    增加了对微服务是否可用的心跳检测，当发现服务down掉时，会移除服务

--- microservice-privoider-dept-8002/src/main/java/com/ldw/microservice/DeptProvider8002_App.java
+++ microservice-privoider-dept-8002/src/main/java/com/ldw/microservice/DeptProvider8002_App.java
@@ -2,14 +2,14 @@ package com.ldw.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
+@EnableFeignClients
 public class DeptProvider8002_App {
-	public static void main(String[] args) {
-		SpringApplication.run(DeptProvider8002_App.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider8002_App.class, args);
+    }
 }
