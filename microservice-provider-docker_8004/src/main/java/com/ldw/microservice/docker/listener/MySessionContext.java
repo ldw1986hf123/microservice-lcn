2e1bc106fb7aea62b994a653402b3c6390f060b6
ldw <ldw1986hf123@163.com>
2019-12-19 01:18:37 +0800

    session test

---
+++ microservice-provider-docker_8004/src/main/java/com/ldw/microservice/docker/listener/MySessionContext.java
@@ -0,0 +1,23 @@
+package com.ldw.microservice.docker.listener;
+
+import java.util.HashMap;
+import javax.servlet.http.HttpSession;
+
+public class MySessionContext {
+    private static HashMap<String, HttpSession> mymap = new HashMap<String, HttpSession>();
+    public static synchronized void AddSession(HttpSession session) {
+        if (session != null) {
+            mymap.put(session.getId(), session);
+        }
+    }
+    public static synchronized void DelSession(HttpSession session) {
+        if (session != null) {
+            mymap.remove(session.getId());
+        }
+    }
+    public static synchronized HttpSession getSession(String session_id) {
+        if (session_id == null)
+            return null;
+        return mymap.get(session_id);
+    }
+}
---
+++ microservice-provider-docker_8004/src/main/java/com/ldw/microservice/docker/listener/SessionConfiguration.java
@@ -0,0 +1,18 @@
+package com.ldw.microservice.docker.listener;
+
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class SessionConfiguration extends WebMvcConfigurerAdapter {
+    //注册session监听器;
+    @Bean
+    public ServletListenerRegistrationBean<SessionListener> servletListenerRegistrationBean() {
+        ServletListenerRegistrationBean<SessionListener> slrBean = new ServletListenerRegistrationBean<SessionListener>();
+        slrBean.setListener(new SessionListener());
+        return slrBean;
+    }
+
+}
