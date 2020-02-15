9fca0b1233360b25acedff496a1f2c345afedb82
ldw <ldw1986hf123@163.com>
2020-02-16 02:24:02 +0800

    fix gateway

---
+++ microservice-gateway/src/main/java/com/microservice/gateway/filter/TokenFilter.java
@@ -0,0 +1,33 @@
+//package com.microservice.gateway.filter;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.commons.lang3.StringUtils;
+//import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+//import org.springframework.cloud.gateway.filter.GlobalFilter;
+//import org.springframework.core.Ordered;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//
+//
+///**
+// * 全局过滤器
+// */
+//@Slf4j
+//public class TokenFilter implements GlobalFilter, Ordered {
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//        String token = exchange.getRequest().getQueryParams().getFirst("token");
+//        if (StringUtils.isEmpty(token)) {
+//            log.info("token 为空，请求非法");
+//            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+//            return exchange.getResponse().setComplete();
+//        }
+//        return chain.filter(exchange);
+//    }
+//
+//    @Override
+//    public int getOrder() {
+//        return -100;
+//    }
+//}
