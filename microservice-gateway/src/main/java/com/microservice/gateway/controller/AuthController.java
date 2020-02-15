c77ce8e3d98893a2d555302df8cb6f40b200cecf
ldw <ldw1986hf123@163.com>
2020-02-16 02:21:50 +0800

    fix gateway

--- microservice-gateway/src/main/java/com/microservice/gateway/controller/AuthController.java
+++ microservice-gateway/src/main/java/com/microservice/gateway/controller/AuthController.java
@@ -3,11 +3,17 @@ package com.microservice.gateway.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 /**
  * 描述: 认证接口
@@ -20,6 +26,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    private @Autowired
+    DiscoveryClient discoveryClient;
+
     /**
      * 登陆认证接口
      *
@@ -29,9 +38,22 @@ public class AuthController {
     @RequestMapping("/login")
     public void login() {
         System.out.println("login");
-
-
     }
 
+
+    @RequestMapping("getServiceList")
+    public List getServiceList() {
+        List list = discoveryClient.getServices();
+        System.out.println("service list " + list);
+
+        List<ServiceInstance> listServiceInstance = discoveryClient
+                .getInstances("MICROSERVICE-DEPT");
+        for (ServiceInstance serviceInstance : listServiceInstance) {
+            System.out.println("getServiceId:" + serviceInstance.getServiceId()
+                    + "\t getHost: " + serviceInstance.getHost()
+                    + "\t  getPort:" + serviceInstance.getPort());
+        }
+        return listServiceInstance;
+    }
 
 }
