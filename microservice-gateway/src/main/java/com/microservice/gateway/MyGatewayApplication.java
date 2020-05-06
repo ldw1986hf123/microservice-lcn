3977bae79d35de7cf3b5cd83276af593cad516bc
ldw <ldw1986hf123@163.com>
2020-05-07 02:07:50 +0800

    fix gateway  限流

--- microservice-gateway/src/main/java/com/microservice/gateway/MyGatewayApplication.java
+++ microservice-gateway/src/main/java/com/microservice/gateway/MyGatewayApplication.java
@@ -1,8 +1,12 @@
 package com.microservice.gateway;
 
+import com.microservice.gateway.resolver.HostAddrKeyResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.cloud.gateway.filter.ratelimit.RateLimiter;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @SpringBootApplication springboot启动类注解
@@ -16,4 +20,9 @@ public class MyGatewayApplication {
         SpringApplication.run(MyGatewayApplication.class, args);
     }
 
+
+    @Bean(name = HostAddrKeyResolver.BEAN_NAME)
+    public HostAddrKeyResolver hostAddrKeyResolver() {
+        return new HostAddrKeyResolver();
+    }
 }
