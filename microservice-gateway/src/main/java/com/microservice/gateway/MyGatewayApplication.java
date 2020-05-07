5189cf4ff1c95530d436aae2f1f1ebc8e9da1110
ldw <ldw1986hf123@163.com>
2020-05-08 01:34:36 +0800

    fix gateway

--- microservice-gateway/src/main/java/com/microservice/gateway/MyGatewayApplication.java
+++ microservice-gateway/src/main/java/com/microservice/gateway/MyGatewayApplication.java
@@ -1,12 +1,16 @@
 package com.microservice.gateway;
 
 import com.microservice.gateway.resolver.HostAddrKeyResolver;
+import com.microservice.gateway.resolver.UriKeyResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.gateway.filter.ratelimit.RateLimiter;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import reactor.core.publisher.Mono;
 
 /**
  * @SpringBootApplication springboot启动类注解
@@ -21,8 +25,20 @@ public class MyGatewayApplication {
     }
 
 
-    @Bean(name = HostAddrKeyResolver.BEAN_NAME)
+    @Primary
+    @Bean
     public HostAddrKeyResolver hostAddrKeyResolver() {
         return new HostAddrKeyResolver();
+    }
+
+
+    @Bean
+    public UriKeyResolver uriKeyResolver() {
+        return new UriKeyResolver();
+    }
+
+    @Bean
+    KeyResolver apiKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("userId"));
     }
 }
