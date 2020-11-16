c8e95807685fe227ffd7e3c9dcce85edb67dfdff
ldw <ldw1986hf123@163.com>
2020-11-17 01:08:28 +0800

    fix gateway

--- microservice-elasticsearch/src/test/java/ProductTest.java
+++ microservice-elasticsearch/src/test/java/ProductTest.java
@@ -1,6 +1,8 @@
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.anqi.es.DemoEsApplication;
-import com.anqi.es.entity.DateUtils;
 import com.anqi.es.entity.Product;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.get.GetRequest;
@@ -26,7 +28,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Date;
 
 @SpringBootTest(classes = DemoEsApplication.class)
@@ -42,11 +43,16 @@ public class ProductTest {
     @Test
     public void addDoc() {
         // 1.准备需要保存到索引库的Json文档数据
+        Date today = new Date();
+
         for (int i = 0; i < 50; i++) {
             Product product = new Product(Long.valueOf(i), "小米手机", "手机", "小米", Double.valueOf(100 * i),
-                    "http://www.baidu.com", DateUtils.stringToDate(), new Date());
+                    "http://www.baidu.com", DateUtil.offsetDay(today, (0 - 2 * i)), new Date());
             // 2.将对象转为Json字符串
             String jsonString = JSON.toJSONString(product);
+
+            System.out.println("插入的记录：  " + jsonString);
+
             // 3.创建请求对象,指定索引库、类型、id(可选)
             IndexRequest indexRequest = new IndexRequest(index);
             // 4.调用source方法将请求数据封装到IndexRequest请求对象中
@@ -104,7 +110,10 @@ public class ProductTest {
             SearchHits hits = response.getHits();
             System.out.println("总共有" + hits.getHits().length + "条记录");
             for (SearchHit hit : hits) {
-                System.out.println(hit.getSourceAsString());
+                JSONObject jsonObject = JSONObject.parseObject(hit.getSourceAsString());
+                Long millseconds = jsonObject.getLong("updatedTime");
+                System.out.print("updateTime: " + DateUtil.format(new Date(millseconds), DatePattern.UTC_SIMPLE_PATTERN) + "---" + jsonObject);
+                System.out.println();
             }
         } catch (IOException e) {
             // TODO Auto-generated catch block
@@ -112,21 +121,28 @@ public class ProductTest {
         }
     }
 
-    public String search() throws IOException {
+    @Test
+    public void search() throws IOException {
         BoolQueryBuilder boolBuilder = QueryBuilders.boolQuery();
         SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
 //        MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchQuery("fields.entity_id", "319");//这里可以根据字段进行搜索，must表示符合条件的，相反的mustnot表示不符合条件的
-         RangeQueryBuilder rangeQueryBuilder = QueryBuilders.rangeQuery("fields_timestamp"); //新建range条件
-         rangeQueryBuilder.gte("2019-03-21T08:24:37.873Z"); //开始时间
-         rangeQueryBuilder.lte("2019-03-21T08:24:37.873Z"); //结束时间
-        // boolBuilder.must(rangeQueryBuilder);
+        RangeQueryBuilder rangeQueryBuilder = QueryBuilders.rangeQuery("updatedTime"); //新建range条件
+        Long startDate = new Date().getTime();
+        Long endDate = DateUtil.offsetDay(new Date(), -3).toJdkDate().getTime();
+
+
+        System.out.println(startDate);
+        System.out.println(endDate);
+
+        rangeQueryBuilder.gte(endDate); //开始时间   1605542257234
+        // 1605286255697     1605545455697
+        rangeQueryBuilder.lte(startDate); //结束时间
+        boolBuilder.must(rangeQueryBuilder);
 //        boolBuilder.must(matchQueryBuilder);
         sourceBuilder.query(boolBuilder); //设置查询，可以是任何类型的QueryBuilder。
         sourceBuilder.from(0); //设置确定结果要从哪个索引开始搜索的from选项，默认为0
         sourceBuilder.size(100); //设置确定搜素命中返回数的size选项，默认为10
 //        sourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS)); //设置一个可选的超时，控制允许搜索的时间。
-
-
 //        sourceBuilder.fetchSource(new String[]{"fields.port", "fields.entity_id", "fields.message"}, new String[]{}); //第一个是获取字段，第二个是过滤的字段，默认获取全部
         SearchRequest searchRequest = new SearchRequest(index); //索引
         searchRequest.source(sourceBuilder);
@@ -134,9 +150,11 @@ public class ProductTest {
         SearchHits hits = response.getHits();  //SearchHits提供有关所有匹配的全局信息，例如总命中数或最高分数：
         SearchHit[] searchHits = hits.getHits();
         for (SearchHit hit : searchHits) {
-            System.out.println(hit);
+            JSONObject jsonObject = JSONObject.parseObject(hit.getSourceAsString());
+            Long millseconds = jsonObject.getLong("updatedTime");
+            System.out.print("updateTime: " + DateUtil.format(new Date(millseconds), DatePattern.UTC_SIMPLE_PATTERN) + "---" + jsonObject);
+            System.out.println();
         }
-        return Arrays.toString(searchHits);
     }
 
 
@@ -157,5 +175,6 @@ public class ProductTest {
         }*/
         client.indices().create(request, RequestOptions.DEFAULT);
     }
+
 
 }
