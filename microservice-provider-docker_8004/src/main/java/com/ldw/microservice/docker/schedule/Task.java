4f34cda7d12f1df351c56da4d5e1988892bd1fa7
ldw1986hf123@163.com <19861020123abc>
2021-05-23 22:39:55 +0800

    add redission

--- microservice-provider-docker_8004/src/main/java/com/ldw/microservice/docker/schedule/Task.java
+++ microservice-provider-docker_8004/src/main/java/com/ldw/microservice/docker/schedule/Task.java
@@ -1,9 +1,14 @@
 package com.ldw.microservice.docker.schedule;
 
-import cn.hutool.core.date.DateUtil;
+import com.ldw.microservice.docker.constnat.RedisKeyPrefixConstant;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author WangXf
@@ -14,21 +19,43 @@ import org.springframework.stereotype.Component;
 @PersistJobDataAfterExecution
 @DisallowConcurrentExecution
 @Component
-public class Task implements Job {
+public class Task implements InterruptableJob {
 
+    @Autowired
+    private RedissonClient redissonClient;
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        log.info("{} 执行", DateUtil.now());
-        for (int i = 0; i < 300; i++) {
-            log.info("执行第{}次",i);
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        String lockKey = RedisKeyPrefixConstant.LOCK_COURSE;
+        RLock lock = redissonClient.getLock(lockKey);
+        try {
+            boolean res = lock.tryLock();
+            if (res) {
+                log.info("拿到了锁");
+                for (int i = 0; i < 20; i++) {
+                    log.info("执行第{}次", i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
+                }
+            } else {
+                log.info("没用拿到锁");
+                return;
+            }
+        }/* catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/ finally {
+            log.info("释放了锁");
+            lock.unlock();
         }
+
     }
 
+    @Override
+    public void interrupt() throws UnableToInterruptJobException {
+
+    }
 }
