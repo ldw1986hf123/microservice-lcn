76e5e517eeff44b6abcc0867fc6bb5cab03b81f6
ldw1986hf123@163.com <19861020123abc>
2021-05-23 22:37:50 +0800

    add redission

--- microservice-provider-docker_8004/src/main/java/com/ldw/microservice/docker/schedule/ScannerAutoExecutionTask.java
+++ microservice-provider-docker_8004/src/main/java/com/ldw/microservice/docker/schedule/ScannerAutoExecutionTask.java
@@ -3,8 +3,7 @@ package com.ldw.microservice.docker.schedule;
 import cn.hutool.core.date.DateUtil;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
-import org.quartz.JobDataMap;
-import org.quartz.Scheduler;
+import org.quartz.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,7 +11,11 @@ import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
+
+import static org.quartz.JobBuilder.newJob;
 
 /**
  * ClassName:ScannerAutoExecutionTask <br/>
@@ -38,10 +41,10 @@ public class ScannerAutoExecutionTask {
      */
 //    @Async
 //    @Scheduled(cron = "0 */1 * * * ?")
-    public void execute() {
-        try {
+    public void execute() throws ParseException, SchedulerException {
+    /*    try {
             log.info("{}----------开始任务清理", DateUtil.now());
-            String cronExpress = "0 */1 * * * ?";
+            String cronExpress = "0 26,29,33 * * * ?";
             String taskCode = "taskCode_1";
             JobDetailFactoryBean jobDetailFactoryBean = new JobDetailFactoryBean();
             jobDetailFactoryBean.setName(taskCode);
@@ -68,7 +71,26 @@ public class ScannerAutoExecutionTask {
             log.info("{}----------结束任务清理", DateUtil.now());
         } catch (Exception e) {
             log.error(" {} 任务检查失败 !", DateUtil.now(), e);
-        }
+        }*/
+
+        // 2. 定义一个Trigger，定义该job在4秒后首次执行，并且每隔两秒执行一次
+        Date startTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2015-09-17 22:40:00");
+        startTime.setTime(startTime.getTime());
+        SimpleTrigger trigger = (SimpleTrigger) TriggerBuilder.newTrigger()
+                .withIdentity("myTrigger", "group1")// 定义名字和组
+                .startAt(startTime)//定义开始时间
+                .withSchedule(    //定义任务调度的时间间隔和次数
+                        SimpleScheduleBuilder
+                                .simpleSchedule()
+                                .withIntervalInMinutes(5)//定义时间间隔是 5分钟
+                                .withRepeatCount(SimpleTrigger.REPEAT_INDEFINITELY)//定义重复执行次数是无限次
+                )
+                .build();
+        //1. 创建一个JodDetail实例 将该实例与Hello job class绑定    (链式写法)
+        JobDetail jobDetail = newJob(Task.class) // 定义Job类为HelloQuartz类，这是真正的执行逻辑所在
+                .withIdentity("myJob") // 定义name/group
+                .build();
+        scheduler.scheduleJob(jobDetail, trigger);
     }
 
     /**
