a4b95932605356c1d90fbc3c80fc71a88ec4f8ff
ldw1986hf123 <ldw1986hf123@163.com>
2025-03-11 04:39:30 +0800

    用docker-compose统一管理各个服务

--- script/make_image_push_ali.sh
+++ script/make_image_push_ali.sh
@@ -1,7 +1,8 @@
+cd /home/steven/microservice-uc
 mvn clean package -DskipTests
 docker build -t ldw_microservice/microservice-uc:latest .
 docker push ldw_microservice/microservice-uc:latest
 
 # 拉取新镜像并重启 service-user
-docker-compose pull service-user
-docker-compose up -d service-user
+docker-compose pull microservice-uc
+docker-compose up -d microservice-uc
