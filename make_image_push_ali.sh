eac5a00aac3c0a68d59bf916ece95c84f64943f8
ldw1986hf123 <ldw1986hf123@163.com>
2025-03-16 08:16:53 +0800

    用docker的mysql做数据库

--- make_image_push_ali.sh
+++ make_image_push_ali.sh
@@ -1,16 +1,16 @@
-cd /home/steven/microservice-uc
+cd /home/steven/$1
 git pull
 mvn clean package -DskipTests
-docker build -t microservice-uc:latest .
+docker build -t $1:latest .
 
 #docker login --username=ldw1986hf123@163.com crpi-4p5cpj1jkb2orw5h.cn-hangzhou.personal.cr.aliyuncs.com
 #docker push ldw_microservice/microservice-uc:latest
 
 # 拉取新镜像并重启 service-user
 #docker-compose pull microservice-uc
-cd /home/steven/microservice-lcn
+cd /home/steven/$1
 
 # shellcheck disable=SC2016
-echo '$1' 重新打包启动
-docker-compose up -d '$1'   #与docker-compose.yml中定义的service对应
-tail -500f /home/steven/microservice-uc/logs/myapp-info.log
+echo $1 重新打包启动
+docker-compose up -d $1   #与docker-compose.yml中定义的service对应
+tail -500f /home/steven/$1/logs/myapp-info.log
