4791e95bc1c513a775e20fe911de1c4f15037439
ldw1986hf123 <ldw1986hf123@163.com>
2025-07-12 17:04:51 +0800

    脚本遇到错误就停止，不再往下执行

--- make_image_push_ali.sh
+++ make_image_push_ali.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 PROJECT_DIR="/home/ldw/code"
 
 # 脚本遇到任何错误就退出
@@ -7,7 +8,7 @@ git pull
 
 echo '更新对应的项目'  $1
 
-cd "$PROJECT_DIR"/$1
+cd "$PROJECT_DIR/$1"
 git pull
 mvn clean package -DskipTests
 docker build -t $1:latest .
@@ -17,7 +18,7 @@ docker build -t $1:latest .
 
 # 拉取新镜像并重启 service-user
 #docker-compose pull microservice-uc
-cd "$PROJECT_DIR"/microservice-lcn
+cd "$PROJECT_DIR/microservice-lcn"
 
 # shellcheck disable=SC2016
 echo $1 重新打包启动
